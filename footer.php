1cb60bbc41d489613457e0831e707ce8c093d1c6
Zeek-kull <wemperado004@gmail.com>
2025-06-19 16:19:32 +0800

    Fixed some bug

--- footer.php
+++ footer.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="zxx">
 
+<?php $title = "A & M Clothing"; ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="description" content="Fashi Template">
     <meta name="keywords" content="Fashi, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Fashi | Template</title>
+   
+    <title><?php echo $title; ?></title>
+
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
