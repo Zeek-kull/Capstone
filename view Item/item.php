48e1d2aae109b49c55fe3bee4a47adbf11e68880
Zeek-kull <wemperado004@gmail.com>
2025-06-19 16:24:05 +0800

    fixed item title

--- view Item/item.php
+++ view Item/item.php
@@ -12,7 +12,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <title>Shop Item - Start Bootstrap Template</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Bootstrap icons-->
