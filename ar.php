948ca399bbd0cfde3d7ba3fc231b42a04dde3e53
Maruko88 <paduam451@gmail.com>
2025-09-14 03:05:45 +0800

    ar

---
+++ ar.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Snap AR Integration</title>
+  </head>
+  <body>
+    <div style="width: 100%; height: 100vh; display: flex; justify-content: center; align-items: center;">
+      <canvas id="canvas" style="width: 100%; height: auto;"></canvas>
+    </div>
+    <script type="module" src="js/camera.js"></script>
+  </body>
+</html>
