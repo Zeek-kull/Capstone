a1f9def9ce1ab9ea22b60d71d0f75cd17ff08eef
Zeek-kull <wemperado004@gmail.com>
2025-09-18 04:40:01 +0800

    fix tab design

--- product.php
+++ product.php
@@ -261,10 +261,10 @@ if (isset($_POST['add_to_cart'])) {
                                 <div class="product-tab">
                                     <div class="tab-item">
                                     <ul class="nav" role="tablist" aria-label="Product information tabs">
-                                        <li class="nav-item" role="presentation">
+                                        <li>
                                             <a id="tab-1-tab" class="nav-link active" data-toggle="tab" href="#tab-1" role="tab" aria-controls="tab-1" aria-selected="true" tabindex="0">DESCRIPTION</a>
                                         </li>
-                                        <li class="nav-item" role="presentation">
+                                        <li>
                                             <a id="tab-2-tab" class="nav-link" data-toggle="tab" href="#tab-2" role="tab" aria-controls="tab-2" aria-selected="false" tabindex="-1">SIZE CHART</a>
                                         </li>
                                     </ul>
